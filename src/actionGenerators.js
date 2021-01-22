8f92ee8d07b7acaee761918c86319ef320289d1f
Rayhaan Khalid <rkhalid@me.com>
2021-01-22 14:54:55 +0000

    add edit to readme, comment out redux thunk practice

--- src/actionGenerators.js
+++ src/actionGenerators.js
@@ -1,15 +1,16 @@
 import * as actions from './actions'
-import axios from 'axios'
+// import axios from 'axios'
+
 let counter = 0
-// export function todoAdded(todo){
-//     return {
-//         type: actions.ADD_TODO,
-//         payload: {
-//             todo,
-//             id: ++counter
-//         }
-//     }
-// }
+export function todoAdded(todo){
+    return {
+        type: actions.ADD_TODO,
+        payload: {
+            todo,
+            id: ++counter
+        }
+    }
+}
 
 export function todoRemover(id){
     return {
@@ -21,28 +22,27 @@ export function todoRemover(id){
     }
 }
 
-// redux-thunk/async version
+// redux-thunk/async version practice
 
-export function todoAdded(todo){
-    return async (dispatch, getState) => {
-        console.log(getState())
+// export function todoAdded(todo){
+//     return async (dispatch, getState) => {
+//         console.log(getState())
 
-          const resp = await axios.post('https://jsonplaceholder.typicode.com/posts')
-          
-          if (!resp.status >= 200 && resp.status < 300) {
-            console.log('db interaction complete');
-              dispatch({
-                  type: actions.ADD_TODO,
-                  payload: {
-                      todo,
-                      id: ++counter
-                    }
-                })
-            }
-            else {
-                console.log('lol didn\'t work')
-            }
+//           const resp = await axios.post('https://jsonplaceholder.typicode.com/posts')
+//           if (resp.status >= 200 && resp.status < 300) {
+//             console.log('db interaction complete');
+//               dispatch({
+//                   type: actions.ADD_TODO,
+//                   payload: {
+//                       todo,
+//                       id: ++counter
+//                     }
+//                 })
+//             }
+//             else {
+//                 console.log('lol didn\'t work')
+//             }
 
-        console.log(getState()) // the result will now be updated with the state, as the dispatch has taken place.
-    }
-}
+//         console.log(getState()) // the result will now be updated with the state, as the dispatch has taken place.
+//     }
+// }
